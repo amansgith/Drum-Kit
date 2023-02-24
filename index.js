5c7aacd188696876930a08edf7bd789b01451035
Aman Khan <amankhancompq2002@gmail.com>
2023-02-24 21:59:29 +0530

    Add files via upload
    Added the files of project

---
+++ index.js
@@ -0,0 +1,67 @@
+
+let arr = document.querySelectorAll(".drum");
+
+for (let i = 0; i < arr.length; i++) {
+    //for keyboard eventlistener
+    arr[i].addEventListener("keydown", function(event){
+        soundmaking(event.key);
+        animate(event.key);
+    });
+    
+    //for mouse eventlistener
+    arr[i].addEventListener("click", function (){
+        let kickButton=this.innerHTML;//to get theletter clicked
+
+        soundmaking(kickButton);
+        animate(kickButton);
+    });
+
+    
+    //function for event key check and sound play
+    function soundmaking(key) {
+        switch (key) {
+            case "a":
+                var audio= new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio= new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio= new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "f":
+                var audio= new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio= new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio= new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio= new Audio("sounds/crash.mp3");
+                audio.play();
+                break;                        
+            default:
+                alert("please enter valid key :(");
+                break;
+        }
+    }
+
+    //for adding click animation
+    function animate(key) {
+        let activekey= document.querySelector("." + key);
+        activekey.classList.add("pressed");
+        
+        setTimeout(function (){
+            activekey.classList.remove("pressed");
+        }, 100)
+    }
+}
+
